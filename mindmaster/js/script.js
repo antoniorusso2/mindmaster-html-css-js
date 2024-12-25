91d4aac346308113a00492a73c3b9a248bf89687
antoniorusso2 <russoantonio303@outlook.it>
2024-12-25 20:12:11 +0100

    - rules for modal alignment

--- mindmaster/js/script.js
+++ mindmaster/js/script.js
@@ -1,4 +1,4 @@
-console.log("mindmaster");
+console.log('mindmaster');
 
 //gioco nel quale bisogna indovinare 4 numeri generati casualmente
 //array con quattro numeri casuali
@@ -16,7 +16,7 @@ function getRandomIntInclusive(min, max) {
  * @param {string} content
  * @returns
  */
-function createDOMElement(tag, classList = [], content = "") {
+function createDOMElement(tag, classList = [], content = '') {
   const element = document.createElement(tag);
 
   if (classList.length > 0) {
@@ -26,24 +26,17 @@ function createDOMElement(tag, classList = [], content = "") {
 
   return element;
 }
+
 function winOrLose(bool) {
   if (bool) {
-    const youWin = createDOMElement("h2", [], "Hai Vinto!");
-    const winText = createDOMElement(
-      "p",
-      [],
-      "I numeri inseriti sono giusti e nella posizione corretta"
-    );
+    const youWin = createDOMElement('h2', [], 'Hai Vinto!');
+    const winText = createDOMElement('p', [], 'I numeri inseriti sono giusti e nella posizione corretta');
 
     modalTitle.appendChild(youWin);
     modalText.appendChild(winText);
   } else {
-    const youLose = createDOMElement("h2", [], "Hai Perso!");
-    const loseText = createDOMElement(
-      "p",
-      [],
-      "Hai finito il numero di tentativi validi, prova di nuovo!"
-    );
+    const youLose = createDOMElement('h2', [], 'Hai Perso!');
+    const loseText = createDOMElement('p', [], 'Hai finito il numero di tentativi validi, prova di nuovo!');
 
     modal.appendChild(youLose);
     modal.appendChild(loseText);
@@ -63,23 +56,23 @@ function removeAddClass(el, remove, add) {
 
 //DOM
 //input html per l'inserimento dei numeri
-const inputNums = document.querySelectorAll(".user-num"); //nodeList con i numeri inseriti
-const form = document.getElementById("input-form"); //form evento submit
-const num1 = document.getElementById("num1");
-const num2 = document.getElementById("num2");
-const num3 = document.getElementById("num3");
-const num4 = document.getElementById("num4");
+const inputNums = document.querySelectorAll('.user-num'); //nodeList con i numeri inseriti
+const form = document.getElementById('input-form'); //form evento submit
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const num3 = document.getElementById('num3');
+const num4 = document.getElementById('num4');
 // modale
-const modalContainer = document.querySelector(".modal-container");
+const modalContainer = document.querySelector('.modal-container');
 console.log(modalContainer);
-const modal = document.querySelector(".modal");
+const modal = document.querySelector('.modal');
 // console.log(modal);
-const modalTitle = document.querySelector(".modal-header");
-const modalText = document.querySelector(".modal-body");
-const playAgainBtn = document.querySelector(".btn.modal-btn");
+const modalTitle = document.querySelector('.modal-header');
+const modalText = document.querySelector('.modal-body');
+const playAgainBtn = document.querySelector('.btn.modal-btn');
 
 //button
-const btn = document.querySelector(".btn");
+const btn = document.querySelector('.btn');
 console.log(btn);
 
 //===================================================================//
@@ -97,21 +90,21 @@ while (numsToGuess.length < 4) {
 }
 
 //pulizia campi alla ricarica della pagina
-num1.value = "";
-num2.value = "";
-num3.value = "";
-num4.value = "";
+num1.value = '';
+num2.value = '';
+num3.value = '';
+num4.value = '';
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const userGuesses = [];
   const rightGuesses = [];
   //lista risposte date dall'utente
-  const answerList = document.querySelector(".answers-list");
+  const answerList = document.querySelector('.answers-list');
   // console.log(answerList.length);
   //contenitore badges
-  const badgesContainer = createDOMElement("div", ["badges-container"]);
+  const badgesContainer = createDOMElement('div', ['badges-container']);
 
   for (let i = 0; i < numsToGuess.length; i++) {
     //trasformazione in numero intero il valore inserito dall'utente;
@@ -122,45 +115,33 @@ form.addEventListener("submit", (event) => {
 
     //* controllo uguaglianza e posizione numero
     if (numsToGuess[i] === userNum) {
-      const correctBadge = createDOMElement(
-        "div",
-        ["correct-badge", "fa-regular", "fa-circle"],
-        ""
-      );
+      const correctBadge = createDOMElement('div', ['correct-badge', 'fa-regular', 'fa-circle'], '');
       badgesContainer.appendChild(correctBadge);
 
       rightGuesses.push(userNum);
     } else if (numsToGuess.includes(userNum)) {
-      const wrongPosition = createDOMElement(
-        "div",
-        ["wrong-position", "fa-regular", "fa-square"],
-        ""
-      );
+      const wrongPosition = createDOMElement('div', ['wrong-position', 'fa-regular', 'fa-square'], '');
       badgesContainer.appendChild(wrongPosition);
     } else {
-      const wrong = createDOMElement(
-        "div",
-        ["wrong", "fa-solid", "fa-xmark"],
-        ""
-      );
+      const wrong = createDOMElement('div', ['wrong', 'fa-solid', 'fa-xmark'], '');
       badgesContainer.appendChild(wrong);
     }
 
     // inputNums[i].value = ''; //?pulizia campo
   }
 
-  const answer = createDOMElement("li", ["answer"], userGuesses);
+  const answer = createDOMElement('li', ['answer'], userGuesses);
   answerList.appendChild(answer);
 
   // aggiunta del container dei badge
   answer.appendChild(badgesContainer);
 
   if (rightGuesses.length === numsToGuess.length) {
-    removeAddClass(modalContainer, "hidden", "flex");
+    removeAddClass(modalContainer, 'hidden', 'flex');
     winOrLose(true);
     modalContainer.appendChild(modal);
   }
-  playAgainBtn.addEventListener("click", () => {
+  playAgainBtn.addEventListener('click', () => {
     location.reload();
   });
 });
